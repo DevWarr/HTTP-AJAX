c4f21ae04bd45536b44345d423f9ed5aae4a8517
Devin Warrick <dev.warr@gmail.com>
2019-06-12 14:35:21 -0500

    Friendlist.js now maps through Friend.js. Setting up props now...

---
+++ friends/src/components/Friend.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Friend = (props) =>{
+    return (
+        <div>Empty space</div>
+    );
+}
+
+export default Friend;
--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,11 +1,12 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
+import Friend from "./Friend";
 
 export default class FriendList extends React.Component {
     constructor() {
         super();
         this.state = {
-            friendData: []
+            friendsArray: []
         };
     }
 
@@ -24,7 +25,8 @@ export default class FriendList extends React.Component {
         axios
             .get("http://localhost:5000/friends")
             .then(res => {
-                console.log("It worked!");
+                console.log(res);
+                this.setState({ friendsArray:res.data })
             })
             .catch(err => {
                 console.error("It didn't work!", err);
@@ -34,7 +36,12 @@ export default class FriendList extends React.Component {
 
     render() {
         return (
-            <div>Empty space</div>
+            <div className="main-container">
+                <h1>The Friend Databse</h1>
+                {this.state.friendsArray.map(friendObj => {
+                    return <Friend friend={friendObj} key={friendObj.id} />
+                })}
+            </div>
         );
     }
 }
