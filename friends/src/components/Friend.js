7d1a35d8371c33a903d11eb912cf7e1c6fe9942a
Devin Warrick <dev.warr@gmail.com>
2019-06-12 15:39:32 -0500

    Working on retro-style database styling.

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./FriendList.scss";
 
 const Friend = (props) =>{
     return (
-        <div classname="friend-card">
+        <div className="friend-card">
             <h3>{props.friend.name}</h3>
             <ul>
                 <li>{props.friend.age}</li>
